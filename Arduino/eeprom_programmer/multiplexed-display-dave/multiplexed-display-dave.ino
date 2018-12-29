a553aed5950c83d1597161ba5411eef564c71673
David Clifford <david.clifford.142@gmail.com>
2018-12-29 20:10:40 +1100

    add hex, octal and alpha

--- Arduino/eeprom_programmer/multiplexed-display-dave/multiplexed-display-dave.ino
+++ Arduino/eeprom_programmer/multiplexed-display-dave/multiplexed-display-dave.ino
@@ -9,6 +9,20 @@
 #define EEPROM_D7 12
 #define WRITE_EN 13
 
+#define D0 0x0
+#define D1 0x100
+#define D2 0x200
+#define D3 0x300
+#define NEGATIVE 0x400
+#define HEXA 0x800
+#define OCTAL 0xc00
+#define ALPHA 0x1000
+
+#define BLANK 0x0
+#define NEG 0x1
+#define LTR_H 0x37
+#define LTR_O 0x1d
+
 /*
    Output the address bits and outputEnable signal using shift registers.
 */
@@ -58,7 +72,6 @@ void writeEEPROM(int address, byte data) {
   delay(10);
 }
 
-
 /*
    Read the contents of the EEPROM and print them to the serial monitor.
 */
@@ -78,6 +91,22 @@ void printContents() {
   }
 }
 
+int digit(int value, int place, int div) {
+  byte digits[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b }; // with 0 for zero
+
+  int p = place & 0x3FF;
+  int digit = digits[abs(value/div)%10];
+  if(value>=0) {
+    if(value<10 && p==D1) digit = BLANK;
+    if(value<100 && p==D2) digit = BLANK;
+    if(p==D3) digit = BLANK;
+  } else {
+    if(abs(value)<10 && p==D1) digit = (value<0) ? NEG : BLANK;
+    if(abs(value)<100 && p==D2) digit = (value<=-10) ? NEG : BLANK;
+    if(p==D3) digit = (value<=-100) ? NEG : BLANK; 
+  }
+  return digit;
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -88,35 +117,76 @@ void setup() {
   pinMode(WRITE_EN, OUTPUT);
   Serial.begin(57600);
 
-  // Bit patterns for the digits 0..9
-  byte digits0[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b }; // with 0 for zero
-  byte digits[] = { 0x00, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b }; // with blank for zero
-
   Serial.println("Programming unsigned numbers");
   for (int value = 0; value <= 255; value += 1) {
-    writeEEPROM(value, digits0[value % 10]);
-    writeEEPROM(value + 256, digits[(value / 10) % 10]);
-    writeEEPROM(value + 512, digits[(value / 100) % 10]);
-    writeEEPROM(value + 768, 0);
+    writeEEPROM((byte)value | D0, digit(value, D0, 1));
+    writeEEPROM((byte)value | D1, digit(value, D1, 10));
+    writeEEPROM((byte)value | D2, digit(value, D2, 100));
+    writeEEPROM((byte)value | D3, digit(value, D3, 1));
     if(value%16==0) Serial.print(".");
   }
   Serial.println();
   
   Serial.println("Programming signed numbers");
   for (int value = -128; value <= 127; value += 1) {
-    writeEEPROM((byte)value + 1024, digits0[abs(value) % 10]);
-    writeEEPROM((byte)value + 1280, digits[abs(value / 10) % 10]);
-    writeEEPROM((byte)value + 1536, digits[abs(value / 100) % 10]);
-    writeEEPROM((byte)value + 1792, value < 0 ? 0x01 : 0);
+    writeEEPROM((byte)value | NEGATIVE | D0, digit(value, D0, 1));
+    writeEEPROM((byte)value | NEGATIVE | D1, digit(value, D1, 10));
+    writeEEPROM((byte)value | NEGATIVE | D2, digit(value, D2, 100));
+    writeEEPROM((byte)value | NEGATIVE | D3, digit(value, D3, 1));
     if(value%16==0) Serial.print(".");
   }
   Serial.println();
 
+  //byte hex[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b, 0x7d, 0x1f, 0x0d, 0x3d, 0x6f, 0x47 }; //lowercase
+  byte hex[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b, 0x77, 0x1f, 0x4e, 0x3d, 0x4f, 0x47 }; // uppercase
+  Serial.println("Programming hex numbers");
+  for (int value = 0; value <= 255; value += 1) {
+    writeEEPROM((byte)value | HEXA | D0, hex[(value%16)]);
+    writeEEPROM((byte)value | HEXA | D1, hex[((value/16)%16)]);
+    writeEEPROM((byte)value | HEXA | D2, BLANK);
+    writeEEPROM((byte)value | HEXA | D3, LTR_H);
+    if(value%16==0) Serial.print(".");
+  }
+  Serial.println();
+
+  byte digits[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70}; // 0-7
+  Serial.println("Programming octal numbers");
+  for (int value = 0; value <= 255; value += 1) {
+    writeEEPROM((byte)value | OCTAL | D0, digits[(value%8)]);
+    writeEEPROM((byte)value | OCTAL | D1, digits[((value/8)%8)]);
+    writeEEPROM((byte)value | OCTAL | D2, digits[((value/64)%8)]);
+    writeEEPROM((byte)value | OCTAL | D3, LTR_O);
+    if(value%16==0) Serial.print(".");
+  }
+  Serial.println();
+
+  byte letters[] = { BLANK, 0x7d, 0x1f, 0x0d, 0x3d, 0x6f, 0x47, 0x7b, //space, a-g
+                      0x17, 0x50, 0x58, 0x37, 0x0e, 0x4f, 0x15, 0x1d, // h-o
+                      0x67, 0x73, 0x05, 0x19, 0x0f, 0x1c, 0x0c, 0x79, // p-w
+                      0x37, 0x3b, 0x25, 0x80, 0xb0, 0xe5, 0x08, 0x01}; //x-z, . ! ? _ - 
+  Serial.println("Programming alpha");
+  for (int disp = 0; disp<4; disp++) {
+    for (int value = 0; value <= 32; value += 1) {
+      int let1 = BLANK;
+      int let2 = BLANK;
+      int let3 = BLANK;
+      int let4 = BLANK;
+      if (disp==0) let1 = letters[value];
+      if (disp==1) let2 = letters[value];
+      if (disp==2) let3 = letters[value];
+      if (disp==3) let4 = letters[value];
+      writeEEPROM((byte)value+disp*32 | ALPHA | D3, let1); 
+      writeEEPROM((byte)value+disp*32 | ALPHA | D2, let2); 
+      writeEEPROM((byte)value+disp*32 | ALPHA | D1, let3); 
+      writeEEPROM((byte)value+disp*32 | ALPHA | D0, let4); 
+      if(value%16==0) Serial.print(".");
+    }
+  }
+  Serial.println();
   // Read and print out the contents of the EERPROM
   Serial.println("Reading EEPROM");
   printContents();
 }
-
 
 void loop() {
   // put your main code here, to run repeatedly:
