887c3e29fe733a91187aac7f264ef4684c42c19a
davidclifford <david.clifford.142@gmail.com>
2019-04-27 17:17:42 +1000

    Add most instructions

--- Python/eeprom-programmer/eeprom-programmer.py
+++ Python/eeprom-programmer/eeprom-programmer.py
@@ -33,11 +33,48 @@ DI = uint32(1) << 26  # D reg in
 DO = uint32(1) << 27  # D reg out
 SI = uint32(1) << 28  # Stack Pointer in
 SO = uint32(1) << 29  # Stack Pointer out
-DM = uint32(1) << 30  # Display mode in (dec/signed/hex/octal/dascii)
+IO = uint32(1) << 30  # Input reg out
 HL = uint32(1) << 31  # Halt CPU (not needed?)
 
 OPERAND = PO | MI | PC
 FETCH = OPERAND | IL
+
+ALU_CLR = 0
+ALU_BSUB = S0
+ALU_SUB = S1
+ALU_ADD = S0 | S1
+ALU_XOR = S2
+ALU_OR = S2 | S0
+ALU_AND = S2 | S1
+ALU_SET = S2 | S1 | S0
+
+MOV = 0x10
+LD = 0x20
+ST = 0x30
+ADD = 0x40
+ADC = 0x50
+SUB = 0x60
+SBB = 0x70
+OR = 0x80
+XOR = 0x90
+AND = 0xA0
+CMP = 0xB0
+
+NOT = 0xC0
+NEG = 0xC4
+INC = 0xC8
+DEC = 0xCC
+
+IN = 0xD0
+OUT = 0xD4
+
+XXrr = 0xD8
+YYrr = 0xDC
+
+LSL = 0xE0
+LSR = 0xE4
+ROL = 0xE8
+ROR = 0xEC
 
 # print("{:08X}".format(IL|PO|PC|MI|HL))
 
@@ -60,28 +97,90 @@ def _w(dd):
 
 
 # ss dd rr 00 A 01 B 10 C
-def instruction(addr, *micro):
-    for f in range(4):
-        instr[addr][0][f] = FETCH
-    i = 0
+def instruction_c_f(address, carry, flags, *micro):
+    f = 1 if flags else 0
+    f = f+2 if carry else f
+    instr[address][0][f] = FETCH
+    t = 0
     for m in micro:
-        i += 1
-        for f in range(4):
-            instr[addr][i][f] = m
-    instr[addr][i][0] = instr[addr][i][0] | TR
-    for bl in range(i+1, 8):
-        for f in range(4):
-            instr[addr][bl][f] = 0
+        t += 1
+        instr[address][t][f] = m
+    instr[address][t][0] = instr[address][t][0] | TR
+    for bl in range(t+1, 8):
+        instr[address][bl][f] = 0
+
+
+def instruction_c(address, carry, *micro):
+    instruction_c_f(address, carry, False, *micro)
+    instruction_c_f(address, carry, True, *micro)
+
+
+def instruction(address, *micro):
+    instruction_c(address, False, *micro)
+    instruction_c(address, True, *micro)
+
+
+def binary_instructions():
+    for dd in range(4):
+        for ss in range(4):
+            if dd == ss:
+                instruction(MOV | dd << 2 | ss, OPERAND, RO | _w(dd))
+                instruction(LD | dd << 2 | ss, OPERAND, RO | MI, RO | _w(dd))
+                instruction(ST | dd << 2 | ss, OPERAND, RI | _r(dd))
+                instruction(ADD | dd << 2 | ss, OPERAND, RO | YI, _r(dd) | XI, ALU_ADD | EO | _w(dd) | FL)
+                instruction_c(ADC | dd << 2 | ss, False, OPERAND, RO | YI, _r(dd) | XI, ALU_ADD | EO | _w(dd) | FL)
+                instruction_c(ADC | dd << 2 | ss, True, OPERAND, RO | YI, _r(dd) | XI, ALU_ADD | CY | EO | _w(dd) | FL)
+                instruction(SUB | dd << 2 | ss, OPERAND, RO | YI, _r(dd) | XI, ALU_SUB | CY | EO | _w(dd) | FL)
+                instruction_c(SBB | dd << 2 | ss, False, OPERAND, RO | YI, _r(dd) | XI, ALU_SUB | CY | EO | _w(dd) | FL)
+                instruction_c(SBB | dd << 2 | ss, True, OPERAND, RO | YI, _r(dd) | XI, ALU_SUB | EO | _w(dd) | FL)
+                instruction(AND | dd << 2 | ss, OPERAND, RO | YI, _r(dd) | XI, ALU_AND | EO | _w(dd) | FL)
+                instruction(OR | dd << 2 | ss, OPERAND, RO | YI, _r(dd) | XI, ALU_OR | EO | _w(dd) | FL)
+                instruction(XOR | dd << 2 | ss, OPERAND, RO | YI, _r(dd) | XI, ALU_XOR | EO | _w(dd) | FL)
+                instruction(CMP | dd << 2 | ss, OPERAND, RO | YI, _r(dd) | XI, ALU_SUB | FL)
+            else:
+                instruction(MOV | dd << 2 | ss, _r(ss) | _w(dd))
+                instruction(LD | dd << 2 | ss, _r(ss) | MI, RO | _w(dd))
+                instruction(ST | dd << 2 | ss, _r(ss) | MI, RI | _r(dd))
+                instruction(ADD | dd << 2 | ss, _r(ss) | YI, _r(dd) | XI, ALU_ADD | EO | _w(dd) | FL)
+                instruction_c(ADC | dd << 2 | ss, False, _r(ss) | YI, _r(dd) | XI, ALU_ADD | EO | _w(dd) | FL)
+                instruction_c(ADC | dd << 2 | ss, True, _r(ss) | YI, _r(dd) | XI, ALU_ADD | CY | EO | _w(dd) | FL)
+                instruction(SUB | dd << 2 | ss, _r(ss) | YI, _r(dd) | XI, ALU_SUB | CY | EO | _w(dd) | FL)
+                instruction_c(SBB | dd << 2 | ss, False, _r(ss) | YI, _r(dd) | XI, ALU_SUB | CY | EO | _w(dd) | FL)
+                instruction_c(SBB | dd << 2 | ss, True, _r(ss) | YI, _r(dd) | XI, ALU_SUB | EO | _w(dd) | FL)
+                instruction(AND | dd << 2 | ss, _r(ss) | YI, _r(dd) | XI, ALU_AND | EO | _w(dd) | FL)
+                instruction(OR | dd << 2 | ss, _r(ss) | YI, _r(dd) | XI, ALU_OR | EO | _w(dd) | FL)
+                instruction(XOR | dd << 2 | ss, _r(ss) | YI, _r(dd) | XI, ALU_XOR | EO | _w(dd) | FL)
+                instruction(CMP | dd << 2 | ss, _r(ss) | YI, _r(dd) | XI, ALU_SUB | FL)
+
+
+def unary_instructions():
+    for rr in range(4):
+        instruction(NOT | rr, _r(rr) | XI, ALU_SET | EO | YI, ALU_XOR | EO | _w(rr) | FL)
+        instruction(NEG | rr, _r(rr) | XI, Y0 | ALU_BSUB | EO | _w(rr) | FL)
+        instruction(INC | rr, _r(rr) | XI, Y0 | CY | ALU_ADD | EO | _w(rr) | FL)
+        instruction(DEC | rr, _r(rr) | XI,  Y0 | ALU_SUB | EO | _w(rr) | FL)
+        instruction(IN | rr, IO | _w(rr))
+        instruction(OUT | rr, _r(rr) | OI)
+        instruction(LSL | rr, _r(rr) | XI | YI, ALU_ADD | EO | _w(rr) | FL)
+        instruction(LSR | rr, _r(rr) | RV | XI | YI, ALU_ADD | EO | RV | XI | FL, Y0 | ALU_ADD | _w(rr))
+        instruction_c(ROL | rr, False, _r(rr) | XI | YI, ALU_ADD | FL, ALU_ADD | EO | _w(rr) | FL)
+        instruction_c(ROL | rr, True, _r(rr) | XI | YI, ALU_ADD | FL, CY | ALU_ADD | EO | _w(rr) | FL)
+        instruction_c(ROR | rr, False, _r(rr) | RV | XI | YI, ALU_ADD | FL, ALU_ADD | EO | RV | XI | FL, Y0 | ALU_ADD, _w(rr))
+        instruction_c(ROR | rr,  True, _r(rr) | RV | XI | YI, ALU_ADD | FL, CY | ALU_ADD | EO | RV | XI | FL, Y0 | ALU_ADD | EO | _w(rr))
+
+
+def other_instructions():
+    pass
 
 
 def print_all():
     for i in range(256):
         print('instruction: '+'{:02X}'.format(i))
-        for f in range(4):
-            print(('cf', 'cF', 'Cf', 'CF')[f])
-            for t in range(8):
-                print("{:08X}".format(instr[i][t][f]))
-            print()
+        # for f in range(4):
+            # print(('cf', 'cF', 'Cf', 'CF')[f])
+        for t in range(8):
+            print("{:08X}".format(instr[i][t][0]))
+        print()
 
 
 def init_all_nop():
@@ -92,14 +191,9 @@ def init_all_nop():
 
 def main():
     init_all_nop()
-
-    # move instructions 0x10
-    for dd in range(4):
-        for ss in range(4):
-            if dd == ss:
-                instruction(0x00 | dd << 2 | ss, OPERAND, RO | _w(dd))
-            else:
-                instruction(0x00 | dd << 2 | ss, _r(ss) | _w(dd))
+    binary_instructions()
+    unary_instructions()
+    other_instructions()
 
     print_all()
 
